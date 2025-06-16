3a562b8ad32d3909948d99a44c7246bac00030de
architectPhilemon <mwendwaphilemon84@gmail.com>
2025-06-17 00:41:24 +0300

    Update frontend API URL for deployment

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5002/api',
+  baseURL: 'https://book-clinic-production.up.railway.app/api',
 });
 
 export default api;
