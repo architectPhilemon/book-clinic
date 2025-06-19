7ad726a61b7291916beaea2454906582915e3ccd
architectPhilemon <mwendwaphilemon84@gmail.com>
2025-06-19 23:25:58 +0300

    Fix userService.js to use shared api instance and correct endpoints

--- frontend/src/services/userService.js
+++ frontend/src/services/userService.js
@@ -1,5 +1,6 @@
 import api from '../api/api';
 
+// Use this for deployed backend (api base URL is set in api.js)
 export const register = async (userData) => {
   const res = await api.post('/users/register', userData);
   return res.data;
